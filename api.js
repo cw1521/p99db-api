c1dc11d520c232429345d80c4807b6b47f2d4bcf
cw1521 <cw1521@jagmail.southalabama.edu>
2020-05-14 13:21:20 -0500

    cors test

--- api.js
+++ api.js
@@ -16,7 +16,7 @@ const corsOptions = {
 server = express();
 
 
-server.use(cors(corsOptions));
+server.use(cors());
 
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(bodyParser.json());
@@ -25,9 +25,9 @@ server.use(bodyParser.json());
 server.use("*", function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
-    //res.header("Content-Type", "application/json");
-    //console.log(req.params);
+    // res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
+    res.header("Content-Type", "application/json");
+    console.log(req.params);
     if ("OPTIONS" === req.method) { 
       return res.status(200);
     }
