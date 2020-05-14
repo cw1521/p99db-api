580c04f8c58f2fb4b3c2fe72d0fba4e2193bcd28
cw1521 <cw1521@jagmail.southalabama.edu>
2020-05-14 14:57:52 -0500

    cors test

--- api.js
+++ api.js
@@ -24,13 +24,14 @@ server.use(bodyParser.json());
 
 server.use("*", function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     // res.header("Content-Type", "application/json");
     console.log(req.params);
-    // if ("OPTIONS" === req.method) { 
-    //   return res.status(200);
-    // }
+    if ("OPTIONS" === req.method) { 
+      
+      res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+      return res.status(200).json({});
+    }
     next();
   });
 
